620a4531d9a24c81bd30b6390544f6f823cc1302
wintered <domwinterer@gmail.com>
2021-02-03 15:58:26 +0100

    update & new favicon

--- stats/encrypt.py
+++ stats/encrypt.py
@@ -1,3 +1,3 @@
-privacy_key="e2b463f3e07b70ff937be0fc44039894debc34c9"
+privacy_key="59f49c56f9d2d2e48a3f0b24e07f23a736746709" 
 ta_bug_repo="muchang/type_aware_bugs" 
 sf_bug_repo="muchang/fusion_bugs"
--- stats/test.py
+++ stats/test.py
@@ -1,5 +1,10 @@
 from github import Github
 from encrypt import privacy_key, ta_bug_repo, sf_bug_repo
+import os 
 g = Github(privacy_key)
-repo = g.get_repo("muchang/fusion_bugs")
-print(repo)
+repo = g.get_repo(sf_bug_repo)
+
+for issue in repo.get_issues():
+    labels = [lab.name for lab in issue.labels]
+    if "Confirmed" in labels:
+        os.system("open "+issue.html_url)
