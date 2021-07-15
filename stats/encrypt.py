22517b3aaa79ac109955c6ffc3f4ee690afa5878
wintered <domwinterer@gmail.com>
2021-07-15 20:16:15 +0200

    some fixes

--- stats/encrypt.py
+++ stats/encrypt.py
@@ -1,4 +1,4 @@
-privacy_key="adc9c150a9cb3c4a31d4b0e1b4292472e5824ce0"
+privacy_key="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"
 ta_bug_repo="muchang/type_aware_bugs" 
 sf_bug_repo="muchang/fusion_bugs"
 gta_bug_repo="wintered/typefuzz_bugs"
