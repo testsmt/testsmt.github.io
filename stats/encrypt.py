bb242adaa1ac717cca97a3e6a3392d2cb600e469
wintered <domwinterer@gmail.com>
2021-07-16 11:58:57 +0200

    update

--- stats/encrypt.py
+++ stats/encrypt.py
@@ -1,4 +1,4 @@
-privacy_key="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"
+privacy_key="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"
 ta_bug_repo="muchang/type_aware_bugs" 
 sf_bug_repo="muchang/fusion_bugs"
 gta_bug_repo="wintered/typefuzz_bugs"
